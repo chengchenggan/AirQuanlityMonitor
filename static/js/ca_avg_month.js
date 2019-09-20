c268376ae20e082f2bc4452f7beb824d6e74ef08
kjanghala <spo.kanika@gmail.com>
2019-09-19 18:27:05 -0700

    county colors map

---
+++ static/js/ca_avg_month.js
@@ -0,0 +1,84 @@
+// Creating map object
+var map = L.map("map", {
+  center: [38.7749, -119.4194],
+  zoom: 6
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(map);
+
+// If data.beta.nyc is down comment out this link
+// var link = "http://data.beta.nyc//dataset/0ff93d2d-90ba-457c-9f7e-39e47bf2ac5f/resource/" +
+// "35dd04fb-81b3-479b-a074-a27a37888ce7/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson";
+
+// Uncomment this link local geojson for when data.beta.nyc is down
+var link = "static/data/ca_avg_month.json";
+
+// Function that will determine the color of a neighborhood based on the borough it belongs to
+function chooseColor(aqi) {
+  switch (aqi) {
+  case (aqi < 50):
+    return "green";
+  case (aqi < 100):
+    return "yellow";
+  case (aqi < 150):
+    return "orange";
+  case (aqi < 200):
+    return "orangered";
+  case (aqi < 300):
+    return "red";
+  case (aqi > 300):
+    return "firebrick";
+  default:
+    return "gray";
+  }
+}
+
+// Grabbing our GeoJSON data..
+d3.json(link, function(data) {
+  // Creating a geoJSON layer with the retrieved data
+  L.geoJson(data, {
+    // Style each feature (in this case a neighborhood)
+    style: function(feature) {
+      return {
+        color: "white",
+        // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+        fillColor: chooseColor(feature.properties.aqi),
+        fillOpacity: 0.5,
+        weight: 1.5
+      };
+    },
+    // Called on each feature
+    onEachFeature: function(feature, layer) {
+      // Set mouse events to change map styling
+      layer.on({
+        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+        mouseover: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+        mouseout: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.5
+          });
+        },
+        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+        click: function(event) {
+          map.fitBounds(event.target.getBounds());
+        }
+      });
+      // Giving each feature a pop-up with information pertinent to it
+      layer.bindPopup("<h1>" + feature.properties.county + "</h1> <hr> <h2>" + feature.properties.aqi + "</h2>");
+
+    }
+  }).addTo(map);
+});
---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia21hc29vZCIsImEiOiJjazA3ZzBpZ2ozeWw1M2JwMDFmb2dpaW5qIn0.eatZsRnyw_yh3Qyb2EfOXw";
