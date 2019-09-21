fe6cbbb1304b15c4f9e2f32db31f2b61a76be522
Jane Gan <chengchenggan@gmail.com>
2019-09-20 23:23:14 -0700

    add heatmap

---
+++ static/js/heatmap.js
@@ -0,0 +1,32 @@
+var myMap = L.map("map", {
+  center: [37.7749, -122.4194],
+  zoom: 5
+});
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+var url = "../Resources/ca_by_site_number.json";
+
+d3.json(url, function(response) {
+
+  console.log(response);
+
+  var heatArray = [];
+  for (var j =0; j < 63; j++){
+  for (var i = 0; i < response[j].aqi; i++) {
+      heatArray.push([response[j].latitude, response[j].longitude]);
+    }
+  };
+
+  var heat = L.heatLayer(heatArray, {
+    maxZoom:10,
+    radius: 50,
+    blur: 70
+  }).addTo(myMap);
+
+});
